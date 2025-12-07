72dc44e897936aebfc4f69ddb3d968dd182a3c93
ausdotsn50 <ausdotsn50@gmail.com>
2025-12-07 13:26:18 +0800

    [sol] 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strArr = Array.from(str);
+    return strArr.reverse().join("");
 };
 
 // Do not edit below this line
