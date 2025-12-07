ada70de11b67a386a7364b56b282fc8460673cbd
ausdotsn50 <ausdotsn50@gmail.com>
2025-12-07 13:50:32 +0800

    [sol] 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) { // multiple args
+    return arr.filter(currValue => !args.includes(currValue)) // bool-based remover
 };
 
 // Do not edit below this line
