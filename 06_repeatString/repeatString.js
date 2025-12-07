18f2ac251c09ba9d1622c13749d577c16c63161f
ausdotsn50 <ausdotsn50@gmail.com>
2025-12-07 13:19:49 +0800

    [sol] 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) return "ERROR";
+    
+    const new_str = [];
+    for(let i = 0; i < num; i++) {
+        new_str.push(string)
+    }
+    return new_str.join("");
 };
 
 // Do not edit below this line
