ed767329737a61d215529a304d0cd4ddab701687
ausdotsn50 <ausdotsn50@gmail.com>
2025-12-07 14:18:02 +0800

    [sol] 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(range1, range2) {
+    //console.log(typeof range1 !== 'number');
+    if(range1 < 0 || range2 < 0 || !Number.isInteger(range1) || !Number.isInteger(range2)) {
+        return "ERROR";
+    }
+
+    if(range1 > range2) {
+        let temp = range1;
+        range1 = range2;
+        range2 = temp;
+    }
+
+    let sum = 0;
+    for(let i = range1; i <= range2; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
