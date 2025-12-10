ff9a8cb4df282dd219a994e2aec03d16acb095e5
ausdotsn50 <ausdotsn50@gmail.com>
2025-12-11 05:27:03 +0800

    [sol] 13_ factorial

--- 13_factorial/factorial.js
+++ 13_factorial/factorial.js
@@ -1,5 +1,13 @@
-const factorial = function() {
+const factorial = function(num) {
+    if(num < 0 || !Number.isInteger(num)) {
+        return;
+    }
 
+    if(num == 0 || num == 1) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
 };
 
 // Do not edit below this line
