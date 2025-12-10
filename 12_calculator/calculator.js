a98bf3aae610c19197ea15ed4f7801dd68e009f7
ausdotsn50 <ausdotsn50@gmail.com>
2025-12-11 05:06:06 +0800

    [sol] 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length == 1) return arr[0];
+  
+  return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length == 1) return arr[0];
+  
+  return arr.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num == 0 || num == 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
