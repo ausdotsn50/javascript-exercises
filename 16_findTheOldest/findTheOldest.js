3e6d5e7816948e8a6aaa024c145f4d2238a8506b
ausdotsn50 <ausdotsn50@gmail.com>
2025-12-11 14:26:14 +0800

    [sol] 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const today = new Date().getFullYear();
+    const oldest = arr.sort((a, b) => {
+        if(!a.yearOfDeath) {
+            return (today - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth)
+        } else if(!b.yearOfDeath) {
+            return (a.yearOfDeath - a.yearOfBirth) - (today - b.yearOfBirth)
+        } 
+        else {
+            return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth)
+        }
+    }); 
+    return oldest[arr.length - 1];
 };
 
 // Do not edit below this line
